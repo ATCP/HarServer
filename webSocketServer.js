b76aec4dcd2efc3bce82afbe1f2bc661225c187c
ATCP <poppin.ke@gmail.com>
2017-11-03 16:28:50 -0700

    add webserver

--- webSocketServer.js
+++ webSocketServer.js
@@ -1,20 +1,21 @@
-// http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
 "use strict";
 
 var fs = require('fs');
 const URL = require('url').Url;
 var path = require('path');
+var webServer = require('./webServer.js');
 
 // Optional. You will see this name in eg. 'ps' or 'top' command
 process.title = 'node-chat';
 
-// Port where we'll run the websocket server
-var webSocketsServerPort = 1337;
-
 // websocket and http servers
 var webSocket = require('ws');
-var wss = new webSocket.Server({port: 1337});
-var http = require('http');
+var http = require("http");
+
+
+const server = http.createServer(webServer.app);
+
+const wss = new webSocket.Server({server: server});
 
 
 // list of currently connected clients (users)
@@ -30,7 +31,6 @@ function htmlEntities(str) {
 
 
 
-
 function modify(url) {
     var _url = url.replace(/\/\//g, '-').replace(/\//g, '-');
     console.log(_url);
@@ -42,7 +42,7 @@ var extensions = { };
 // This callback function is called every time someone
 // tries to connect to the WebSocket server
 wss.on('connection', function(ws, res) {
-    console.dir(ws);
+    //console.dir(ws);
 
     console.log((new Date()) + ' Connection from origin ' + res.headers.origin.split('//') + '.');
     console.log((new Date()) + ' Connection from remoteAddress ' + ws._socket.remoteAddress + ':' + ws._socket.remotePort);
@@ -53,18 +53,17 @@ wss.on('connection', function(ws, res) {
     // user sent some message
     ws.on('message', function (message) {
         //console.dir(message);
-        console.log('receive har id %s from host %s', JSON.parse(message).log.creator.name, res.headers.host);
+        console.log('receive har id %s from host %s', JSON.parse(message).log.creator.name, ws._socket.remoteAddress + ':' + ws._socket.remotePort);
 
         var chromeExtension = res.headers.origin.split('//');
         var extensionId = chromeExtension[chromeExtension.length - 1];
 
-	var remoteClient = ws._socket.remoteAddress.split(':');
-	var remoteIP = remoteClient[remoteClient.length - 1];
-	var remotePort = ws._socket.remotePort;
+        var remoteClient = ws._socket.remoteAddress.split(':');
+        var remoteIP = remoteClient[remoteClient.length - 1];
+        var remotePort = ws._socket.remotePort;
 
         var har = JSON.parse(message);
         var tabId = har.log.creator.name;
-
 
         /*
         var requestInfo = JSON.parse(message.utf8Data);
@@ -91,20 +90,37 @@ wss.on('connection', function(ws, res) {
         extensions[extensionId].tabs[tabId].urls.push(tabUrl);
         */
 
-        if (!fs.existsSync(remoteIP + '.' + remotePort)) {
-            fs.mkdir(remoteIP + '.' + remotePort, function (err) {
+        var now = new Date();
+        var logDir = now.getFullYear() + "-"+ now.getMonth() + "-" + now.getDay();
+
+        if (!fs.existsSync(logDir)) {
+            fs.mkdir(logDir, function (err) {
+                if (err) throw (err);
+            })
+        }
+
+
+
+        if (!fs.existsSync(path.join(logDir, remoteIP + '.' + remotePort))) {
+            fs.mkdir(path.join(logDir, remoteIP + '.' + remotePort), function (err) {
                 if (err) throw (err);
             });
         }
 
+        var logDir = path.join(logDir, remoteIP + '.' + remotePort);
 
         //var tabUrl = modify(JSON.parse(message.utf8Data).tabUrl);
-        var tabHarPath = path.join(remoteIP + '.' + remotePort, 'tab-' + tabId + '.har');
+        var tabHarPath = path.join(logDir, 'tab-' + tabId + '.har');
         fs.appendFile(tabHarPath, JSON.stringify(har, null, 2), 'utf8', function (err) {
             if (err) throw err;
         });
 
     });
+});
+
+
+server.listen(process.env.PORT || 1337, function () {
+    console.log('Server started on port %d', server.address().port);
 });
 
 
